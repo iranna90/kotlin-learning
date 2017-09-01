de5b940e6c893c1c227c028952950eab59fe5fe9
iranna.patil@irdeto.com <iranna.patil@irdeto.com>
2017-09-01 17:45:32 +0200

    added function and return from when checks

--- src/learnkotlin/inaction/chapter1/hello/HelloWorld.kt
+++ src/learnkotlin/inaction/chapter1/hello/HelloWorld.kt
@@ -1,5 +1,7 @@
 package learnkotlin.inaction.chapter1.hello
 
+import com.sun.javaws.exceptions.InvalidArgumentException
+
 data class User(val id: Int, val name: String)
 
 fun main(args: Array<String>) {
@@ -13,4 +15,23 @@ fun main(args: Array<String>) {
   } else {
     println("different user")
   }
+
+  println("checking reference")
+  if (user === copiedUserMatching) {
+    println("they have same reference")
+  } else {
+    println("they have different reference")
+  }
+
+  println(add(10,20))
+  println(cal(10,20,"add"))
+  println(cal(10,20,"sub"))
+}
+
+fun add(x: Int, y: Int) = x + y
+
+fun cal(x: Int, y: Int, op: String) = when (op) {
+  "add" -> x + y
+  "sub" -> x - y
+  else -> throw InvalidArgumentException(null)
 }
